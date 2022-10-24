cfc3c3249b8416f8297866ec84a49a7ba64bde4b
Сергей Савельев <carrion19870317@gamil.com>
2022-10-24 15:07:25 +0300

    ДЗ 2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+
+type AppScreenProps = {
+  title: string;
+  genre: string;
+  date: number;
+}
+
+function App({title, genre, date}: AppScreenProps): JSX.Element {
+  return (
+    <WelcomeScreen title={title} genre={genre} date={date}/>
+  );
 }
 
 export default App;
